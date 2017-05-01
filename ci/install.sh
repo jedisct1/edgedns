437faeeefc5ad81e451490def5924ed21ddc9d5a
Frank Denis <github@pureftpd.org>
2017-05-01 14:45:37 +0200

    Travis: no need to call rustlib/uninstall.sh any more

--- ci/install.sh
+++ ci/install.sh
@@ -17,9 +17,6 @@ install_c_toolchain() {
 }
 
 install_rustup() {
-    # uninstall the rust toolchain installed by travis, we are going to use rustup
-    sh ~/rust/lib/rustlib/uninstall.sh
-
     curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
 
     rustc -V
