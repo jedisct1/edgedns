69e7abc85b8a179d31ce50501fcbaff335ec67b2
Frank Denis <github@pureftpd.org>
2017-06-14 21:40:41 +0200

    Rewrite the webservice handler as a Tokio service for Hyper 0.11

--- src/libedgedns/src/webservice.rs
+++ src/libedgedns/src/webservice.rs
@@ -1,10 +1,11 @@
 //! Expose metrics via the Prometheus API
 
-use hyper::header::ContentType;
+use futures::future::{self, FutureResult};
+use hyper;
+use hyper::header::{ContentLength, ContentType};
 use hyper::mime::Mime;
-use hyper::server::{Server, Request, Response};
-use hyper::status::StatusCode;
-use hyper::uri::RequestUri::AbsolutePath;
+use hyper::server::{Http, Service, Server, Request, Response};
+use hyper::{StatusCode, Uri};
 use prometheus::{self, Encoder, TextEncoder};
 use std::io;
 use std::sync::Arc;
@@ -13,25 +14,22 @@ use std::thread;
 use varz::{StartInstant, Varz};
 
 use super::EdgeDNSContext;
-use super::WEBSERVICE_THREADS;
 
+#[derive(Clone)]
 pub struct WebService {
     varz: Arc<Varz>,
 }
 
-impl WebService {
-    fn new(edgedns_context: &EdgeDNSContext) -> WebService {
-        WebService { varz: edgedns_context.varz.clone() }
-    }
+impl Service for WebService {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
 
-    fn handler(&self, req: Request, mut res: Response) {
-        match req.uri {
-            AbsolutePath(ref path) if path == "/metrics" => path,
-            _ => {
-                *res.status_mut() = StatusCode::NotFound;
-                return;
-            }
-        };
+    fn call(&self, req: Request) -> Self::Future {
+        if req.uri().path() != "/metrics" {
+            return future::ok(Response::new().with_status(StatusCode::NotFound));
+        }
         let StartInstant(start_instant) = self.varz.start_instant;
         let uptime = start_instant.elapsed().as_secs();
         self.varz.uptime.set(uptime as f64);
@@ -42,35 +40,42 @@ impl WebService {
         let mut buffer = vec![];
         let encoder = TextEncoder::new();
         encoder.encode(&metric_families, &mut buffer).unwrap();
-        res.headers_mut().set(ContentType(
-                encoder
-                    .format_type()
-                    .parse::<Mime>()
-                    .unwrap(),
-            ));
-        res.send(&buffer).unwrap();
+        future::ok(
+            Response::new()
+                .with_header(ContentLength(buffer.len() as u64))
+                .with_header(ContentType(encoder.format_type().parse::<Mime>().unwrap()))
+                .with_body(buffer)
+        )
+    }
+}
+
+impl WebService {
+    fn new(edgedns_context: &EdgeDNSContext) -> WebService {
+        WebService { varz: edgedns_context.varz.clone() }
     }
 
     pub fn spawn(edgedns_context: &EdgeDNSContext,
                  service_ready_tx: mpsc::SyncSender<u8>)
                  -> io::Result<thread::JoinHandle<()>> {
-        let listen_addr = edgedns_context.config.webservice_listen_addr.to_owned();
+        let listen_addr = edgedns_context
+            .config
+            .webservice_listen_addr
+            .parse()
+            .expect("Unsupport listen address for the prometheus service");
         let web_service = WebService::new(edgedns_context);
         let webservice_th = thread::Builder::new()
             .name("webservice".to_string())
-            .spawn(move || {
-                let mut server =
-                    Server::http(&*listen_addr).expect("Unable to spawn the webservice");
-                server.keep_alive(None);
-                service_ready_tx.send(2).unwrap();
-                info!("Webservice started on {}", listen_addr);
-                server
-                    .handle_threads(
-                        move |req: Request, res: Response| web_service.handler(req, res),
-                        WEBSERVICE_THREADS,
-                    )
-                    .expect("Unable to start the webservice");
-            })
+            .spawn(
+                move || {
+                    let server = Http::new()
+                        .keep_alive(false)
+                        .bind(&listen_addr, move || Ok(web_service.clone()))
+                        .expect("Unable to spawn the webservice");
+                    service_ready_tx.send(2).unwrap();
+                    info!("Webservice started on {}", listen_addr);
+                    server.run().expect("Unable to start the webservice");
+                }
+            )
             .unwrap();
         Ok(webservice_th)
     }
