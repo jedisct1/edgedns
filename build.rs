b87044c5f3d41a46e6c9f7e2874241990d0820fb
Frank Denis <github@pureftpd.org>
2017-09-17 15:54:04 +0200

    Update deps

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["src/edgedns-cli/edgedns-cli.proto"],
-                                &["src/edgedns-cli"]).unwrap();
+    prost_build::compile_protos(&["src/edgedns-cli/edgedns-cli.proto"], &["src/edgedns-cli"])
+        .unwrap();
 }
